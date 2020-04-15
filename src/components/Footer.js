2f1bbd7e99dec0bd455fb779cb0a3105ec2efea4
Maxime Princelle <maxime@princelle.org>
2020-04-15 14:21:43 +0200

    Update Modules

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,37 @@
 import React, { Component } from 'react';
 export default class Footer extends Component {
-  render() {
-    let resumeData = this.props.resumeData;
-    return (
-      <footer>
-      <div className="row">
-        <div className="twelve columns">
-          <ul className="social-links">
-            {
-              resumeData.socialLinks && resumeData.socialLinks.map((item)=>{
-                return(
-                  <li key={item.url}>
-                    <a href={item.url} target="_blank" rel="noopener noreferrer">
-                    <i className={item.className} />
-                    </a>
-                  </li>
-                )
-              })
-            }
-          </ul>
-          
-				</div>
-				
-			<div className="row">
-            <div className="col-full cl-copyright">
-				<span style={{ color: "#D3D3D3"}}>
-					Copyright &copy; {new Date().getFullYear()} | Maxime Princelle
-                </span>
-            </div>
-        </div>
-				
-        <div id="go-top" style={{ display: "none" }}><a className="smoothscroll" title="Back to Top" href="#nav-bar"><i className="icon-up-open" /></a></div>
-      </div>
-    </footer>
-    );
-  }
+    render() {
+        let resumeData = this.props.resumeData;
+        return (
+            <footer>
+                <div className="row">
+                    <div className="twelve columns">
+                        <ul className="social-links">
+                            {
+                                resumeData.socialLinks && resumeData.socialLinks.map((item)=>{
+                                    return(
+                                        <li key={item.url}>
+                                            <a href={item.url} target="_blank" rel="noopener noreferrer">
+                                                <i className={item.className} />
+                                            </a>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-full cl-copyright">
+                            <span style={{ color: "#D3D3D3"}}>
+                                Copyright &copy; {new Date().getFullYear()} | Maxime Princelle
+                            </span>
+                        </div>
+                    </div>
+
+                    <div id="go-top" style={{ display: "none" }}><a className="smoothscroll" title="Back to Top" href="#nav-bar"><i className="icon-up-open" /></a></div>
+                </div>
+            </footer>
+        );
+    }
 }
